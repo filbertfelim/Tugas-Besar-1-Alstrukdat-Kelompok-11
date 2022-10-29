2877a4b0485a8528a85dbfd3267608e51c154bd6
filbertfelim24 <110520631+filbertfelim24@users.noreply.github.com>
2022-10-29 14:28:43 +0700

    Add files via upload

---
+++ src/ADT/Mesinkar_kata/mesin_driver.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "mesin_kata.h"
+
+// membaca file konfigurasi
+int main()
+{
+    STARTWORD();
+    while (!EndWord)
+    {
+        for (int i = 0; i < currentWord.Length; i++)
+        {
+            printf("%c", currentWord.TabWord[i]);
+        }
+        printf("\n");
+        ADVWORD();
+    }
+    return 0;
+}
