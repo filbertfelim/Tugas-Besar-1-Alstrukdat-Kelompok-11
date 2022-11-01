3af4883e40a13de4643a382382e223d4a54a91a3
Adrenalin Apprizal <18221125@std.stei.itb.ac.id>
2022-11-01 11:30:50 +0700

    Update rng.c

--- src/GAMERNG/rng.c
+++ src/GAMERNG/rng.c
@@ -2,7 +2,7 @@
 #include "time.h"
 #include <stdlib.h>
 
-/*Setiap permainan dimulai dengan program sudah menentukan sebuah angka acak X.
+/*
 Di setiap giliran, pemain diberi kesempatan menebak angka X. Game akan memberi tahu apakah tebakan pemain dibandingkan terhadap X lebih besar atau lebih kecil.
 Permainan selesai jika pemain menebak angka X dengan benar.
 Skor untuk game ini tergantung dengan seberapa cepat pemain menebak X. Formula skor dibebaskan.
@@ -10,17 +10,22 @@ Batasan X dan maksimal giliran dibebaskan.
 */
 
 int rng()
+// I.S. Setiap permainan dimulai dengan program sudah menentukan sebuah angka acak X.
+// F.S. Program selesai apabila pemain berhasil menebak angka x dengan benar. Skor bergantung pada jumlah tebakan pemain
+// Semakin banyak jumlah tebakan maka skor yang didapat akan semakin kecil
 {
-    int tebakan;
+    int tebakan, skor; //tebakan adalah angka yang ditebak, skor adalah skor yang didapat
+    int max = 100; //maksimal tebakan
 
     printf("RNG Telah dimulai. Uji kebenaran Anda dengan menebak X.\n");
     printf("Tebakan: ");
-    scanf("%d", &tebakan);
-    srand(time(NULL));
+    scanf("%d", &tebakan); //memasukkan tebakan
+    srand(time(NULL)); //mengacak angka
 
-    int random = rand() % 100;
-    
-    while (tebakan != random){
+    int random = rand() % 100 + 1; //membuat angka random di rentanng 1 - 100
+
+    while (tebakan != random && tebakan <= max) //jika tebakan tidak sama dengan random dan tebakan masih dibawah maksimal)
+    {
         if (tebakan > random){
             printf("Lebih kecil\n");
             printf("Tebakan: ");
@@ -33,9 +38,11 @@ int rng()
             } 
         }
         if (tebakan == random){
-            printf("Ya, x adalah %d\n", random);
+            printf("Ya, X adalah %d\n", random);
         }
-    return 0;
+    skor = max - tebakan; //menghitung skor
+    printf("Skor Anda adalah:  %d\n", skor);
+    return skor;
 }
 
 int main()
