24823113b7c73802ef8735d7f5fd07c56a0c84bb
vaniaasabila <88761105+vaniaasabila@users.noreply.github.com>
2022-11-01 17:10:12 +0700

    Update help.c

--- src/HELP/help.c
+++ src/HELP/help.c
@@ -1,14 +1,14 @@
 void help () {
     printf(" Daftar command:\n");
-    printf("\t 1. STAR       : Menampilkan tampilan awal game. \n");
-    printf("\t 2. LOAD       : Menampilkan list game, histori, dan scoreboard game. \n");
-    printf("\t 3. SAVE       : Menyimpan state game pemain dalam suatu file. \n");
-    printf("\t 4. CREATEGAME : Menambahkan game baru pada daftar game. \n");
-    printf("\t 5. LISTGAME   : Menampilkan daftar game yang disediakan oleh sistem. \n");
-    printf("\t 6. DELETEGAME : Menghapus sebuah game dari daftar. \n");
-    printf("\t 7. QUEUEGAME  : Mendaftarkan game kedalam list game. \n");
-    printf("\t 8. PLAYGAME   : Memainkan sebuah game. \n");
-    printf("\t 9. SKIPGAME   : Melewatkan permainan sebanyak n. \n");
-    printf("\t 10.QUIT       : Keluar dari program game. \n");
+    printf("\t 1. STAR       --- Menampilkan tampilan awal game. \n");
+    printf("\t 2. LOAD       --- Menampilkan list game, histori, dan scoreboard game. \n");
+    printf("\t 3. SAVE       --- Menyimpan state game pemain dalam suatu file. \n");
+    printf("\t 4. CREATEGAME --- Menambahkan game baru pada daftar game. \n");
+    printf("\t 5. LISTGAME   --- Menampilkan daftar game yang disediakan oleh sistem. \n");
+    printf("\t 6. DELETEGAME --- Menghapus sebuah game dari daftar. \n");
+    printf("\t 7. QUEUEGAME  --- Mendaftarkan game kedalam list game. \n");
+    printf("\t 8. PLAYGAME   --- Memainkan sebuah game. \n");
+    printf("\t 9. SKIPGAME   --- Melewatkan permainan sebanyak n. \n");
+    printf("\t 10.QUIT       --- Keluar dari program game. \n");
 }
 
