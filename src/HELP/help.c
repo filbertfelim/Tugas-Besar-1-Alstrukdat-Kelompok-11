91ddb1d78f257eae92eca7fe94fd151325e355ad
vaniaasabila <88761105+vaniaasabila@users.noreply.github.com>
2022-11-01 16:58:50 +0700

    Update help.c

--- src/HELP/help.c
+++ src/HELP/help.c
@@ -1,14 +1,14 @@
 void help () {
     printf(" Daftar command:\n");
-    printf("\t 1. STAR \n");
-    printf("\t 2. LOAD \n");
-    printf("\t 3. SAVE \n");
-    printf("\t 4. CREATEGAME \n");
-    printf("\t 5. LISTGAME \n");
-    printf("\t 6. DELETEGAME \n");
-    printf("\t 7. QUEUEGAME");
-    printf("\t 8. PLAYGAME \n");
-    printf("\t 9. SKIPGAME \n");
-    printf("\t 10. QUIT \n");
+    printf("\t 1. STAR       : Menampilkan tampilan awal game \n");
+    printf("\t 2. LOAD       : Menampilkan list game, histori, dan scoreboard game \n");
+    printf("\t 3. SAVE       : Menyimpan state game pemain dalam suatu file \n");
+    printf("\t 4. CREATEGAME : Menambahkan game baru pada daftar game \n");
+    printf("\t 5. LISTGAME   : Menampilkan daftar game yang disediakan oleh sistem \n");
+    printf("\t 6. DELETEGAME : Menghapus sebuah game dari daftar \n");
+    printf("\t 7. QUEUEGAME  : Mendaftarkan game kedalam list \n");
+    printf("\t 8. PLAYGAME   : Memainkan sebuah game \n");
+    printf("\t 9. SKIPGAME   : Melewatkan permainan sebanyak n \n");
+    printf("\t 10.QUIT       : Keluar dari program game \n");
 }
 
