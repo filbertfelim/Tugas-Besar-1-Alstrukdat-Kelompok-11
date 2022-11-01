ddd400544408b88e8c41bba1de3f804bcb8b8805
rasyadanfz <18221103@std.stei.itb.ac.id>
2022-11-01 12:37:53 +0700

    add queuegame

---
+++ src/QUEUEGAME/queuegame.h
@@ -0,0 +1,12 @@
+#ifndef QUEUEGAME_H
+#define QUEUEGAME_H
+
+#include "../../boolean.h"
+#include "../ADT/Array/array.h"
+#include "../ADT/Queue/queue.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void QueueGame(Queue *GameQueue, TabStr GameList);
+
+#endif
