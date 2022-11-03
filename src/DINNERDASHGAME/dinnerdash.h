bc3e1b146687edc25ddcd1dfa8d17253466af84c
Adrenalin Apprizal <18221125@std.stei.itb.ac.id>
2022-11-03 17:17:48 +0700

    update

--- src/DINNERDASHGAME/dinnerdash.h
+++ src/DINNERDASHGAME/dinnerdash.h
@@ -2,27 +2,10 @@
 #define DINNERDASH_H
 
 #include <stdio.h>
-#include "../ADT/Queue/queue.h"
+#include <queuedinner.h>
 #include <stdlib.h>
 #include "../boolean.h"
 #include <time.h>
-
-#define HARGA 15000
-
-/* Definisi elemen dan address */
-typedef struct {
-    int makanan;
-    int durasimemasak;
-    int ketahanan;
-    int harga;
-} ElType;
-
-/* ********* AKSES (Selektor) ********* */
-/* Jika q adalah Queue, maka akses elemen : */
-#define IDX_HEAD(q) (q).idxHead
-#define IDX_TAIL(q) (q).idxTail
-#define     HEAD(q) (q).buffer[(q).idxHead].idmakanan
-#define     TAIL(q) (q).buffer[(q).idxTail].idmakanan
 
 void ADD(Queue *q); //menambah menu ke queue
 void COOK(Queue qM, Queue *q, int food); //membuat makanan
