a5f7aa09c5915c8a29dce81b6901cf0f305a3189
Adrenalin Apprizal <18221125@std.stei.itb.ac.id>
2022-11-09 17:41:32 +0700

    fix

---
+++ src/BONUS/tictactoe.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "../ADT/Mesinkar_kata/mesin_kata.h"
+
+#ifndef TICTACTOE_H
+#define TICTACTOE_H
+
+void PrintBoard();
+int checkboard();
+void resetboard();
+void playermove();
+void computermove();
+char checkwin();
+void printwin(char winner);
+#endif
